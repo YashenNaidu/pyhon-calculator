896b13f61ed70a8262596352e1538cff31037f41
Cyberhustler <62091903+Cyberhustler@users.noreply.github.com>
2020-04-14 20:36:43 +0200

    Create yashenVersion3.py

---
+++ yashenVersion3.py
@@ -0,0 +1,44 @@
+# Simple calculator
+
+#Function adder
+def adder(x, y):
+   return x + y
+
+# to subtract two numbers
+def minus(x, y):
+   return x - y
+
+# this one will divide the two
+def divide(x, y):
+   return x / y
+
+# this function multi
+def multu(x, y):
+   return x * y
+
+print("Select Yashens Choices fucker.")
+print("1.adder")
+print("2.minus")
+print("3.multu")
+print("4.divide")
+
+# Please enter input
+RyansDevice = input("Enter Your side(4/3/2/1): ")
+
+iNum1 = float(input("Enter your 1st Number: "))
+iNum2 = float(input("Enter your 2st Number: "))
+
+if RyansDevice == '1':
+    print(iNum1, "+", iNum2, "=", adder(iNum1, iNum2))
+elif RyansDevice == '2':
+    print(iNum1, "-", iNum2, "=", minus(iNum1, iNum2))
+elif RyansDevice == '3':
+    print(iNum1, "/", iNum2, "=", multu(iNum1, iNum2))
+elif RyansDevice == '4':
+    if iNum2 == 0 :
+        print ("Yashen Fixed it!")
+    else:
+        print(iNum1, "*", iNum2, "=", divide(iNum1, iNum2))
+
+else:
+        print("Invalid input")
